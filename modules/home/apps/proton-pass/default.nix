2eb29e4d249c1bc53879f074a8a0c9b544a93811
Minh Hieu Tran <hieu.tran21198@gmail.com>
2026-06-04 01:09:09 +0700

    feat: integrate proton-pass with librewolf

--- modules/home/apps/proton-pass/default.nix
+++ modules/home/apps/proton-pass/default.nix
@@ -27,8 +27,34 @@
         in
         {
           apps = {
+            # moz-extension://962aca5c-4c9f-4088-ae7c-8d74cc1463d4/manifest.json
             librewolf.extensions.${firefoxExt.addonId} = lib.mkIf apps.proton-pass.integrateLibrewolf {
               package = firefoxExt;
+              settings = {
+                permissions = [
+                  "activeTab"
+                  "alarms"
+                  "scripting"
+                  "storage"
+                  "unlimitedStorage"
+                  "webNavigation"
+                  "webRequest"
+                  "webRequestBlocking"
+                  "https://account.proton.me/*"
+                  "https://pass.proton.me/*"
+
+                  # optional permissions
+                  "privacy"
+                  "webRequestAuthProvider"
+                  "clipboardRead"
+                  "clipboardWrite"
+                  "nativeMessaging"
+
+                  # host permissions
+                  "https://*/*"
+                  "http://*/*"
+                ];
+              };
             };
           };
         };
