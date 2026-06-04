ec430f484186803beeea4934d84f66053f48afdb
Minh Hieu Tran <hieu.tran21198@gmail.com>
2026-06-04 13:56:39 +0700

    feat: update home profile

--- homes/x86_64-linux/cirius@mht-home-pc/default.nix
+++ homes/x86_64-linux/cirius@mht-home-pc/default.nix
@@ -5,15 +5,33 @@
       shell = {
         fish.shellAbbrs = {
           rbnix = "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/Workspaces/personal/dotfiles/cirius-nix-v4";
+          gco = "git checkout";
+          gpl = "git pull origin";
+          gps = "git push origin";
+          gaa = "git add .";
+          gcm = "git commit -m";
+          nixgens = "nix-env --list-generations";
+          delgens = "nix-env --delete-generations old";
+          cleanstore = "nix-store --gc";
+        };
+      };
+      desktop-manager = {
+        gnome = {
+          profile = "default";
         };
       };
     };
 
     apps = {
+      aws-cli.enable = true;
       vscodium.enable = true;
       direnv.enable = true;
       devenv.enable = true;
-      proton-pass.enable = true;
+      datagrip.enable = true;
+      gparted.enable = true;
+      proton-pass = {
+        enable = true;
+      };
       ms-teams.enable = true;
       only-office.enable = true;
       ai = {
@@ -25,15 +43,15 @@
         enable = true;
         persistFingerprint = true;
         containers = {
-          default = {
+          uit = {
             id = 1;
             color = "blue";
-            icon = "fingerprint";
+            icon = "briefcase";
           };
           buuuk = {
             id = 2;
             color = "red";
-            icon = "briefcase";
+            icon = "circle";
           };
         };
       };
